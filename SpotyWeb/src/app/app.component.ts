31f2c01f6e30b4150bbb80887200e8dc8518147f
franciscoMonge <mongefranciscojose@gmail.com>
2024-01-11 11:51:43 -0600

    Creación de searchbox y navbar

--- SpotyWeb/src/app/app.component.ts
+++ SpotyWeb/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { SearchBoxComponent } from './shared/components/search-box/search-box.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, SearchBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ SpotyWeb/src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {
+
+}
